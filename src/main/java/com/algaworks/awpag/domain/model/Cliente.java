5c782635796d9679cef4fd197f88676c8d1b6fd8
Romualdo Massuia Junior <romualdo.junior@azi.com.br>
2024-03-24 10:12:07 -0400

    Estudo sobre rest api

---
+++ src/main/java/com/algaworks/awpag/domain/model/Cliente.java
@@ -0,0 +1,39 @@
+package com.algaworks.awpag.domain.model;
+
+import com.algaworks.awpag.domain.validation.ValidationGroups;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Getter
+@Setter
+public class Cliente {
+
+    @NotNull(groups = ValidationGroups.ClienteId.class)
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotBlank
+    @Size(max = 60)
+    private String nome;
+
+    @NotBlank
+    @Size(max = 255)
+    @Email
+    private String email;
+
+    @Size(max = 60)
+    private String fone;
+}
